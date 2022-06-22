aa76b353f1ca94c37a210bf55ccef33b20c08dae
Richard Erdös Chalk <richard.erdos-chalk@sweetsystems.se>
2022-06-22 10:10:02 +0200

    Created key game functions

--- index.js
+++ index.js
@@ -37,11 +37,8 @@ function startGame() {
     // Default direction is right (adding 1 to the array is the same as moving right)
     direction=1;
 
-    if (score > 0) {
-        scoreDisplay.style.color = "green"
-    } else{
-        scoreDisplay.style.color = "rgba(27, 44, 11, 0.8)"
-    }
+    updateScore();
+
     scoreDisplay.innerText = "SCORE: " + score;
     intervalTime = 500;         // Snake moves every half second - 
     currentSnake = [2, 1, 0];   // Set default snake
@@ -52,12 +49,67 @@ function startGame() {
     interval = setInterval(moveOutcomes, intervalTime);
 }
 
-startGame();
-
 function moveOutcomes() {
-    
+    checkForGameEnd();
+    updateSnake();
+    eatsAnApple();
+    updateScore();
+    squares[currentSnake[0]].classList.add("snake");
+}
+
+function checkForGameEnd(){
+    if (
+        (currentSnake[0] % width === width - 1 && direction === 1) ||           // snake head hits right
+        (currentSnake[0] - width < 0 && direction === -width)||                 // snake head hits top
+    (currentSnake[0] % width === 0 && direction === -1) ||                      // snake head hits left
+        (currentSnake[0] + width >= width * width && direction === width) ||    // snake head hits bottom
+        squares[currentSnake[0] + direction].classList.contains("snake")        // snake hits self
+      ) {
+        scoreDisplay.style.color = "red";
+        scoreDisplay.innerText = "Game Over! " + score;
+        return clearInterval(interval);                                         // ends game if any of the above happen
+      }
+}
+
+function updateSnake() {
+    // remove the tail
+    // The pop() method removes the last element from an array
+    const tail = currentSnake.pop();
+    squares[tail].classList.remove("snake");
+
+    // The unshift() method adds one or more elements to the BEGINNING of an array
+    // give direction to the array
+    currentSnake.unshift(currentSnake[0] + direction);
+}
+
+function eatsAnApple(params) {
+    if (squares[currentSnake[0]].classList.contains("apple")) {
+    squares[currentSnake[0]].classList.remove("apple");
+    squares[tail].classList.add("snake");
+    // The push() method adds one or more elements to the END of an array
+    currentSnake.push(tail);
+
+    randomApple();
+    updateScore();
+
+    clearInterval(interval);
+    intervalTime = intervalTime * speed;
+    interval = setInterval(moveOutcomes, intervalTime);
+  }
+}
+
+updateScore(){
+    score++;
+    if (score > 0) {
+      scoreDisplay.style.color = "green";
+    } else {
+      scoreDisplay.style.color = "black";
+    }
+    scoreDisplay.textContent = "My score: " + score;
 }
 
 function randomApple() {
     
 }
+
+startGame();
