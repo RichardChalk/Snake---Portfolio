69dbc1f02b342267d6dca3762cb90e4d141660af
Richard Erdös Chalk <richard.erdos-chalk@sweetsystems.se>
2022-06-22 09:00:57 +0200

    added variables needed

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ function createGrid(){
 }
 
 createGrid();
+
+// Variables neeeded
+const squares = document.querySelectorAll(".grid div");
+const scoreDisplay = document.querySelector("span");
+const startBtn = document.querySelector(".start");
+
+const width = 14;               // Width of my grid
+let currentIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2,1,0]      // Index: 0=tail 1=body 2=head
+
+let direction = 1;              // Number of squares my snake will move
+let score = 0;
+let speed = .85;                // Can be increased to make game more difficult
+let intervalTime = 0;           // Used for timing
+let interval = 0;               // Used for timing
